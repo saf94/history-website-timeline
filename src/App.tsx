d79082514c699c2cf9c207bc397cb2d5917efc23
Safeer Hayat <safeerhayat@IT00738-Saf-Hayat.local>
2022-10-01 13:02:35 +0100

    initial code setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+
+type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
 
 function App() {
+  function onWheel(apiObj: scrollVisibilityApiType, ev: React.WheelEvent): void {
+    const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+    if (isThouchpad) {
+      ev.stopPropagation();
+      return;
+    }
+
+    if (ev.deltaY > 0) {
+      apiObj.scrollNext();
+    } else if (ev.deltaY < 0) {
+      apiObj.scrollPrev();
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScrollMenu
+        onWheel={onWheel}
+      >
+        {data.data.map(element => (
+          <div className='element-wrapper'>
+            <div>
+              <img className="image" alt="google logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/800px-Google_Images_2015_logo.svg.png" />
+            </div>
+            <div>
+              <h3>{element.year}</h3>
+              <p>{element.description}</p>
+            </div>
+          </div>
+        ))}
+      </ScrollMenu >
     </div>
   );
 }
