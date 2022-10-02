8e18ee351d0529038fc7a55b9037b5cf80059048
Safeer Hayat <safeerhayat@IT00738-Saf-Hayat.local>
2022-10-02 11:13:18 +0100

    a bit more styling tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -27,17 +27,19 @@ function App() {
       >
         {data.data.map(element => (
           <div className='timeline-element'>
+            <div className='background-styled-divider'></div>
             <div className='element-wrapper'>
               <div className='image-wrapper'>
                 <img className="image" alt="alt text here" src={element.imageLink} />
               </div>
               <div className='text-wrapper'>
-                <h4>{element.year}</h4>
+                <h4 className='text-year'>{element.year}</h4>
                 <p>{element.description}</p>
               </div>
             </div>
             <div className='timeline-line'></div>
             <div className='timeline-tick'></div>
+            <h4 className='timeline-tick-year'>{element.year}</h4>
           </div>
         ))}
       </ScrollMenu>
