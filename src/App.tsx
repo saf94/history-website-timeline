34aca011ce5db880b844dfc4e5a3fe4b16052f05
Safeer Hayat <safeerhayat@IT00738-Saf-Hayat.local>
2022-10-12 19:45:49 +0100

    add fonts and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css';
-import data from './data.json'
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import "./App.css";
+import data from "./data.json";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 
 type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
 
 function App() {
-  const timelineData = data.data
-  const lengthOfTimeline = timelineData[timelineData.length - 1].year - timelineData[0].year
-  const timelineList = Array(lengthOfTimeline).fill(1).map((x, y) => timelineData[0].year + y)
+  const timelineData = data.data;
+  const lengthOfTimeline =
+    timelineData[timelineData.length - 1].year - timelineData[0].year;
+  const timelineList = Array(lengthOfTimeline)
+    .fill(1)
+    .map((x, y) => timelineData[0].year + y);
 
-  function onWheel(apiObj: scrollVisibilityApiType, ev: React.WheelEvent): void {
+  function onWheel(
+    apiObj: scrollVisibilityApiType,
+    ev: React.WheelEvent
+  ): void {
     const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
 
     if (isThouchpad) {
@@ -26,78 +32,92 @@ function App() {
 
   return (
     <div className="App">
-      <ScrollMenu
-        onWheel={onWheel}
-      >
-        {timelineList.map(year => {
-
-          const timelineElement = timelineData.filter(yearElement => yearElement.year === year)
+      <ScrollMenu onWheel={onWheel}>
+        {timelineList.map((year) => {
+          const timelineElement = timelineData.filter(
+            (yearElement) => yearElement.year === year
+          );
 
           if (timelineElement.length === 0) {
             return (
-              <div className='timeline-element-empty' key={year}>
-                <div className='background-styled-divider'></div>
-                <div className='timeline-element-section' />
-                <div className='timeline-line'></div>
-                <div className='timeline-tick'></div>
-                <h4 className='timeline-tick-year'>{year}</h4>
-                <div className='timeline-element-section' />
+              <div className="timeline-element-empty" key={year}>
+                <div className="background-styled-divider"></div>
+                <div className="timeline-element-section" />
+                <div className="timeline-line"></div>
+                <div className="timeline-tick"></div>
+                <h4 className="timeline-tick-year">{year}</h4>
+                <div className="timeline-element-section" />
               </div>
-            )
+            );
           }
 
           return (
-            <div className='timeline-element' key={year}>
-              <div className='background-styled-divider' />
-              <div className='timeline-element-section top-section'>
+            <div className="timeline-element" key={year}>
+              <div className="background-styled-divider" />
+              <div className="timeline-element-section top-section">
                 {timelineElement.map((element, index) => {
                   if (index % 2 !== 0) {
-                    return null
+                    return null;
                   }
                   return (
-                    <div className='element-wrapper' key={element.description}>
-                      <div className='image-wrapper'>
-                        {/* todo: add alt text */}
-                        <img className="image" alt="alt text here" src={element.imageLink} />
+                    <a href={element.link}>
+                      <div
+                        className="element-wrapper"
+                        key={element.description}
+                      >
+                        <div className="image-wrapper">
+                          {/* todo: add alt text */}
+                          <img
+                            className="image"
+                            alt="alt text here"
+                            src={element.imageLink}
+                          />
+                        </div>
+                        <div className="text-wrapper">
+                          <h4 className="text-year">{element.year}</h4>
+                          <p>{element.description}</p>
+                        </div>
                       </div>
-                      <div className='text-wrapper'>
-                        <h4 className='text-year'>{element.year}</h4>
-                        <p>{element.description}</p>
-                      </div>
-                    </div>
-                  )
+                    </a>
+                  );
                 })}
               </div>
 
-              <div className='timeline-line'></div>
-              <div className='timeline-tick'></div>
-              <h4 className='timeline-tick-year'>{year}</h4>
+              <div className="timeline-line"></div>
+              <div className="timeline-tick"></div>
+              <h4 className="timeline-tick-year">{year}</h4>
 
-              <div className='timeline-element-section bottom-section'>
+              <div className="timeline-element-section bottom-section">
                 {timelineElement.map((element, index) => {
                   if (index % 2 === 0) {
-                    return null
+                    return null;
                   }
                   return (
-                    <div className='element-wrapper' key={element.description}>
-                      <div className='image-wrapper'>
-                        {/* todo: add alt text */}
-                        <img className="image" alt="alt text here" src={element.imageLink} />
+                    <a href={element.link}>
+                      <div
+                        className="element-wrapper"
+                        key={element.description}
+                      >
+                        <div className="image-wrapper">
+                          {/* todo: add alt text */}
+                          <img
+                            className="image"
+                            alt="alt text here"
+                            src={element.imageLink}
+                          />
+                        </div>
+                        <div className="text-wrapper">
+                          <h4 className="text-year">{element.year}</h4>
+                          <p>{element.description}</p>
+                        </div>
                       </div>
-                      <div className='text-wrapper'>
-                        <h4 className='text-year'>{element.year}</h4>
-                        <p>{element.description}</p>
-                      </div>
-                    </div>
-                  )
+                    </a>
+                  );
                 })}
-
               </div>
             </div>
-          )
-        }
-        )
-        }
+          );
+        })}
       </ScrollMenu>
     </div>
   );
